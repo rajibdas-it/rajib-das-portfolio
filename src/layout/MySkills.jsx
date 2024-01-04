55937089c2fd52ac0c637ff8722f6fda3ef0c209
rajibdas-it <rajibrad@gmail.com>
2024-01-04 16:53:05 +0600

    skill section done

---
+++ src/layout/MySkills.jsx
@@ -0,0 +1,41 @@
+import SkillBar from "react-skillbars";
+
+const MySkills = () => {
+  const skills = [
+    { type: "HTML", level: 95 },
+    { type: "CSS", level: 85 },
+    { type: "Bootstrap", level: 60 },
+    { type: "Tailwind", level: 80 },
+    { type: "Ant Design", level: 50 },
+    { type: "React Js", level: 80 },
+    { type: "Next Js", level: 80 },
+    { type: "NodeJs", level: 70 },
+    { type: "Express Js", level: 80 },
+    { type: "MongoDB", level: 70 },
+    { type: "SQL", level: 70 },
+    { type: "Prisma", level: 70 },
+    { type: "Redis", level: 50 },
+    { type: "GraphQL", level: 50 },
+    { type: "AWS", level: 50 },
+    { type: "Docker", level: 50 },
+    { type: "Crystal Report", level: 80 },
+  ];
+
+  // const colors = {
+  //   bar: "#5a68a5",
+  //   title: { text: "#fff", background: "#46465e" },
+  // };
+
+  const colors = {
+    title: { background: "#580E84", text: "#fff" },
+    bar: "linear-gradient(to right,#6471F7, #905EF6, #D44DBC)",
+  };
+
+  return (
+    <div>
+      <SkillBar skills={skills} height={20} colors={colors} />
+    </div>
+  );
+};
+
+export default MySkills;
---
+++ src/layout/SkillsImg.jsx
@@ -0,0 +1,145 @@
+const SkillsImg = () => {
+  return (
+    <div className="animate-skill-spin border-2 border-slate-700 relative lg:w-[390px] lg:h-[390px] rounded-full mx-auto">
+      <div>
+        <img
+          className="absolute top-[80px] left-[4px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/html.svg"
+          alt=""
+        />
+        <img
+          className="absolute top-[29px] left-[48px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/css.svg"
+          alt=""
+        />
+        <img
+          className="absolute top-[-0.3rem] left-[6.5rem]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/bootstrap.svg"
+          alt=""
+        />
+        <img
+          className="absolute -top-[21px] left-[160px]"
+          width={40}
+          height={40}
+          src="/src/assets/images/skills/tailwind.svg"
+          alt=""
+        />
+        <img
+          className="absolute top-[-11px] left-[224px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/javascript.svg"
+          alt=""
+        />
+        <img
+          className="absolute top-[15px] left-[284px]"
+          width={50}
+          height={50}
+          src="/src/assets/images/skills/TypeScript.svg"
+          alt=""
+        />
+        <img
+          className="absolute top-[68px] right-[15px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/react.svg"
+          alt=""
+        />
+        <img
+          className="absolute top-[121px] right-[-11px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/redux.svg"
+          alt=""
+        />
+        <img
+          className="absolute top-[198px] right-[-18px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/next-js-seeklogo.com 1.svg"
+          alt=""
+        />
+
+        <img
+          className="absolute top-[265px] right-[5px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/node-js-icon 1.svg"
+          alt=""
+        />
+        <img
+          className="absolute top-[319px] right-[42px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/express-js.svg"
+          alt=""
+        />
+        <img
+          className="absolute bottom-[-2px] right-[90px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/mongodb-icon 1.svg"
+          alt=""
+        />
+        <img
+          className="absolute bottom-[-15px] left-[197px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/sql-icon 1.svg"
+          alt=""
+        />
+        <img
+          className="absolute bottom-[-15px] left-[126px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/postgresql-icon 1.svg"
+          alt=""
+        />
+        <img
+          className="absolute bottom-[13px] left-[66px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/GraphQL_Logo 1.svg"
+          alt=""
+        />
+        <img
+          className="absolute bottom-[56px] left-[24px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/prisma.svg"
+          alt=""
+        />
+        <img
+          className="absolute bottom-[104px] left-[1px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/redis.svg"
+          alt=""
+        />
+
+        <img
+          className="absolute top-[196px] left-[-16px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/docker-icon 1.svg"
+          alt=""
+        />
+        <img
+          className="absolute top-[143px] left-[-16px]"
+          width={35}
+          height={35}
+          src="/src/assets/images/skills/aws-icon 1.svg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SkillsImg;
