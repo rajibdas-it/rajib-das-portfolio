41873ff9ee9482ae7fdadf4e76ee46b46112e702
rajibdas-it <rajibrad@gmail.com>
2024-02-09 12:19:36 +0600

    create menu for small device

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { RiMenuLine } from "react-icons/ri";
+
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
   return (
-    <div className="">
-      <div className="flex justify-between items-center lg:px-5">
+    <header className="">
+      <section className="flex justify-between items-center lg:px-5">
         <div>
           <p className="text-4xl font-lora italic font-extrabold ml-2 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
             Rajib Das
@@ -52,8 +58,30 @@ const Navbar = () => {
             Contact
           </a>
         </nav>
-      </div>
-    </div>
+
+        <div
+          className="lg:hidden flex items-center"
+          onClick={() => setMenu(!menu)}
+        >
+          {menu ? <AiOutlineClose size={28} /> : <RiMenuLine size={28} />}
+        </div>
+        {/* mobile menu start  */}
+        <div
+          className={`${
+            menu ? "translate-x-0" : "translate-x-[-100%]"
+          } lg:hidden flex flex-col text-center absolute bg-gradient-to-tr from-[#001F3F] to-[#000000] left-0 top-16 text-white text-2xl font-semibold py-8 gap-8 w-full h-fit transition-transform duration-300`}
+        >
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Eductation</a>
+          <a href="">Skills</a>
+          <a href="">Services</a>
+          <a href="">Portfolio</a>
+          <a href="">Contact</a>
+        </div>
+        {/* mobile menu end  */}
+      </section>
+    </header>
   );
 };
 
